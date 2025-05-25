3e51fffb428b0d1177bf01e8c7892b322680243e
mingbaohe <448274855@qq.com>
2025-05-25 22:23:47 +0800

    优化 next.config.js 配置

--- next.config.js
+++ next.config.js
@@ -4,11 +4,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // 确保在 Cloudflare Pages 上正常工作
-  experimental: {
-    appDir: true,
-  },
-  // 添加 trailingSlash 配置
   trailingSlash: true,
 }
 
