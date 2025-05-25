3381afa90e9cc2e4e4299d71ef675956084e7ce7
mingbaohe <448274855@qq.com>
2025-05-25 22:18:03 +0800

    更新 next.config.js 配置以适配 Cloudfare Pages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
   images: {
     unoptimized: true,
   },
+  // 确保在 Cloudflare Pages 上正常工作
+  experimental: {
+    appDir: true,
+  },
+  // 添加 trailingSlash 配置
+  trailingSlash: true,
 }
 
 module.exports = nextConfig 
